bbaba89e5f26cf153fbc9a81e6f717beeb0fe00a
Ivo <izaloudek5@gmail.com>
2024-01-19 17:46:57 +0100

    generate col-ready, now let's fix col size

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,95 @@
-import {useState} from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-    return (
-        <>
-            <Header/>
-            <GridSelect/>
-            <Footer/>
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <BingoGrid />
+    </>
+  );
 }
 
 function Header() {
-    return (
-        <h1 className="text-2xl uppercase py-3 fixed top-0 left-0 right-0">bingo</h1>)
+  return (
+    <h1 className="text-2xl uppercase py-3 fixed top-0 left-0 right-0">
+      bingo
+    </h1>
+  );
 }
 
-function Footer() {
-    return (
-        <footer>
-            <div className="flex space-x-5 items-center justify-center w-1100 py-8">
-                <a>Ivo</a>
-            </div>
-        </footer>
-    )
-}
-
-function GridSelect() {
-    return (
-        <div className="flex-col bg-white p-5 text-black">
-            <form className="flex flex-col gap-3.5">
-                <input className="px-2" type="number" name="boxValue" placeholder="Enter Bingo"/>
-                <button type="button">Create</button>
-            </form>
-        </div>)
+function GridSize({ currentGridNumber, updateGrid, setGridNumber }) {
+  return (
+    <div className="flex flex-row gap-3 content-center py-5 items-center">
+      <h2>Kolik polí má mít Bingo?</h2>
+      <select
+        value={currentGridNumber}
+        onChange={(e) => setGridNumber(Number(e.target.value))}
+      >
+        {[3, 4, 5].map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <button
+        onClick={(e) => {
+          updateGrid();
+        }}
+        className="bg-white text-black"
+      >
+        Confirm
+      </button>
+    </div>
+  );
 }
 
 function BingoGrid() {
-    return (
-        <div className="grid grid-cols-3 gap-3.5">
-            <Box/>
-            <Box/>
-            <Box/>
-            <Box/>
-            <Box/>
-            <Box/>
-            <Box/>
-            <Box/>
-            <Box/>
-        </div>
+  const [boxGrid, setBoxGrid] = useState([]);
+  const [colSize, setColSize] = useState(3);
 
-    )
+  const changeText = (id, newText) => {
+    const arr = boxGrid;
+    arr[id].text = newText;
+    setBoxGrid(arr);
+  };
+
+  const createGrid = () => {
+    setBoxGrid([]);
+    const powerNumber = Math.pow(colSize, 2);
+    const boxArray = [];
+    for (let i = 0; i < powerNumber; i++) {
+      boxArray.push({ id: i, text: "", check: false });
+    }
+    setBoxGrid(boxArray);
+  };
+
+  return (
+    <>
+      <GridSize
+        updateGrid={createGrid}
+        currentGridNumber={colSize}
+        setGridNumber={setColSize}
+      />
+      <div className={`grid grid-cols-${Math.sqrt(boxGrid.length)} gap-4`}>
+        {boxGrid.map((boxData) => {
+          return <Box key={`box-key-${boxData.id}`} boxData={boxData} />;
+        })}
+      </div>
+    </>
+  );
 }
 
-function Box() {
-    return <div
-        className="flex flex-col text-lg place-content-center bg-gray-600 p-2 rounded-md h-32 w-32 hover:bg-sky-700 hover:cursor-pointer">
-        <input className="px-2" type="text" name="boxValue" placeholder="Enter text"/>
+function Box({ boxData }) {
+  return (
+    <div className="bg-white text-black p-3 hover:cursor-pointer rounded-md">
+      <input
+        className="bg-transparent outline outline-1 rounded-md px-3"
+        type="text"
+        placeholder="Enter Value"
+        value="Nazdar"
+      ></input>
     </div>
+  );
 }
-
-export default App
+export default App;
